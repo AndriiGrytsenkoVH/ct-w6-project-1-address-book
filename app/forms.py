73c0d0358c133cfd9378eeefd04e9decdb0e9754
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-11 09:30:45 -0600

    signup

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import EqualTo, InputRequired
 
 class SignUpForm (FlaskForm):
     first_name = StringField("First Name", validators=[InputRequired()])
-    last_name = StringField("Last Name", validators=[InputRequired()])
+    last_name = StringField("Last Name")
     email = EmailField("Email", validators=[InputRequired()])
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
--- app/routes.py
+++ app/routes.py
@@ -9,4 +9,21 @@ def index():
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     form = SignUpForm()
+
+    # print(form.data)
+    if form.validate_on_submit():
+        print('From Submitter and Validated')
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(first_name, last_name, email, username, password)
+        # TODO check if user already exists
+        # if username == 'bs':
+        #     flash('The user already exists', 'danger')
+        #     return render_template('signup.html', form=form)
+        flash('Thank you for signing up!', 'success')
+        return redirect(url_for('index'))
+
     return render_template('signup.html', form=form)
