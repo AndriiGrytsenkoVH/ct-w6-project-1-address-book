e04617c0c9fdc141d420c5da820b02fa4b208e51
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-10 21:02:13 -0600

    skeleton

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import EqualTo, InputRequired
+
+class SignUpForm (FlaskForm):
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
+    email = EmailField("Email", validators=[InputRequired()])
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from app.forms import SignUpForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/signup')
-def index():
-    return render_template('signup.html')
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', form=form)
