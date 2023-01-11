80ac88c4b6806f41aa3b10957c71f7d7de024469
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-10 20:11:18 -0600

    initial commit
    	# new file:   .gitignore
    	# new file:   app/__init__.py
    	# new file:   app/routes.py
    	# new file:   app/templates/alerts.html
    	# new file:   app/templates/base.html
    	# new file:   app/templates/index.html
    	# new file:   app/templates/navbar.html
    	# new file:   app/templates/signup.html
    	# new file:   forms.py
    	# new file:   requirements.txt

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def index():
+    return render_template('signup.html')
